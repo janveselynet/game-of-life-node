8888cbfb1bc7764bd569564bfe40e6d2c56cb84a
Jan Vesely <janvesely@janvesely.net>
2020-11-22 18:56:03 +0100

    add command line interface

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,46 @@
-console.log('Hello world');
+import path from 'path';
+import yargs from 'yargs';
+import { runGame } from './game';
+import { readGameDefinitionFromXml } from './io/xml/xmlReader';
+import { writeWorldToXml } from './io/xml/xmlWriter';
+import { overpopulationRule } from './rules/overpopulationRule';
+import { reproductionRule } from './rules/reproductionRule';
+import { underpopulationRule } from './rules/underpopulationRule';
+
+const DEFAULT_OUTPUT_FILE = path.join(__dirname, '..', 'output.xml');
+
+const rules = [
+  underpopulationRule,
+  overpopulationRule,
+  reproductionRule,
+];
+
+const options = yargs
+  .usage('Usage: -i <inputFile> [-o <outputFile>]')
+  .option(
+    'input',
+    {
+      alias: 'i',
+      describe: 'Input file',
+      type: 'string',
+      demandOption: true,
+    },
+  )
+  .option(
+    'output',
+    {
+      alias: 'o',
+      describe: 'Output file',
+      type: 'string',
+    },
+  )
+  .argv;
+
+(async () => {
+  const gameDefinition = await readGameDefinitionFromXml(options.input);
+
+  const worldAfterEvolution = runGame(gameDefinition, rules);
+
+  const outputFile = options.output || DEFAULT_OUTPUT_FILE;
+  await writeWorldToXml(worldAfterEvolution, outputFile);
+})();
