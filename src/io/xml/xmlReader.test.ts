055e2ee25eea40bb1378b45131c069dade9a8386
Jan Vesely <janvesely@janvesely.net>
2020-11-29 16:05:31 +0100

    add xml input validation

--- src/io/xml/xmlReader.test.ts
+++ src/io/xml/xmlReader.test.ts
@@ -15,4 +15,70 @@ describe('xmlReader', () => {
     expect(gameDefinition.initialWorld.cells[1][0].organismSpecies).toBe(null);
     expect(gameDefinition.initialWorld.cells[1][1].organismSpecies).toBe('3');
   });
+
+  const invalidInputProvider = [
+    {
+      title: 'non-existent file',
+      fileName: 'invalidInputNonExistentFile.xml',
+      expectedErrorMessage: 'Cannot read or parsed input file.',
+    },
+    {
+      title: 'invalid xml file',
+      fileName: 'invalidInputInvalidXml.xml',
+      expectedErrorMessage: 'Cannot read or parsed input file.',
+    },
+    {
+      title: 'invalid xml structure',
+      fileName: 'invalidInputInvalidXmlStructure.xml',
+      expectedErrorMessage: 'Invalid xml structure.',
+    },
+    {
+      title: 'negative number of iterations',
+      fileName: 'invalidInputNegativeNumberOfIterations.xml',
+      expectedErrorMessage: 'Number of iterations must be number bigger than or equal to 0.',
+    },
+    {
+      title: 'negative world size',
+      fileName: 'invalidInputNegativeSize.xml',
+      expectedErrorMessage: 'World size must be number bigger than 0.',
+    },
+    {
+      title: 'zero world size',
+      fileName: 'invalidInputZeroSize.xml',
+      expectedErrorMessage: 'World size must be number bigger than 0.',
+    },
+    {
+      title: 'negative cell x position',
+      fileName: 'invalidInputNegativeXPosition.xml',
+      expectedErrorMessage: 'Cell x position must be number between 0 and world size.',
+    },
+    {
+      title: 'cell x position bigger than size',
+      fileName: 'invalidInputXPositionBiggerThanSize.xml',
+      expectedErrorMessage: 'Cell x position must be number between 0 and world size.',
+    },
+    {
+      title: 'negative cell y position',
+      fileName: 'invalidInputNegativeYPosition.xml',
+      expectedErrorMessage: 'Cell y position must be number between 0 and world size.',
+    },
+    {
+      title: 'cell y position bigger than size',
+      fileName: 'invalidInputYPositionBiggerThanSize.xml',
+      expectedErrorMessage: 'Cell y position must be number between 0 and world size.',
+    },
+  ];
+
+  invalidInputProvider.forEach(test => {
+    it(`throws error on ${test.title}`, async () => {
+      const inputFile = path.join(__dirname, '__fixtures__', test.fileName);
+      expect.assertions(1);
+
+      try {
+        await readGameDefinitionFromXml(inputFile);
+      } catch (error) {
+        expect(error.message).toEqual(test.expectedErrorMessage);
+      }
+    });
+  });
 });
--- src/io/xml/xmlReader.ts
+++ src/io/xml/xmlReader.ts
@@ -2,10 +2,30 @@ import { promises as fs } from 'fs';
 import { parseStringPromise } from 'xml2js';
 import { GameDefinition } from '../../GameDefinition';
 import { Cell, createCellWithSpecies, createEmptyCell } from '../../world/cell/Cell';
+import { InvalidInputError } from '../InvalidInputError';
+
+const validateBasicWorldValues = (numberOfIterations: number, size: number): void => {
+  if (numberOfIterations < 0) {
+    throw new InvalidInputError('Number of iterations must be number bigger than or equal to 0.');
+  }
+  if (size <= 0) {
+    throw new InvalidInputError('World size must be number bigger than 0.');
+  }
+};
+
+const validateCellPosition = (x: number, y: number, worldSize: number): void => {
+  if (x < 0 || x >= worldSize) {
+    throw new InvalidInputError('Cell x position must be number between 0 and world size.');
+  }
+  if (y < 0 || y >= worldSize) {
+    throw new InvalidInputError('Cell y position must be number between 0 and world size.');
+  }
+};
 
 const createGameDefinitionFromParsedData = (parsedData: any): GameDefinition => {
   const numberOfIterations = Number(parsedData.life.world[0].iterations[0]);
   const size = Number(parsedData.life.world[0].cells[0]);
+  validateBasicWorldValues(numberOfIterations, size);
 
   const cells: Cell[][] = [];
   for (let y = 0; y < size; y++) {
@@ -17,6 +37,8 @@ const createGameDefinitionFromParsedData = (parsedData: any): GameDefinition => 
   parsedData.life.organisms[0].organism.forEach((organism: any) => {
     const x = Number(organism.x_pos[0]);
     const y = Number(organism.y_pos[0]);
+    validateCellPosition(x, y, size);
+
     const species = String(organism.species[0]);
     cells[y][x] = createCellWithSpecies(species);
   });
@@ -28,7 +50,22 @@ const createGameDefinitionFromParsedData = (parsedData: any): GameDefinition => 
 };
 
 export const readGameDefinitionFromXml = async (filePath: string): Promise<GameDefinition> => {
-  const data = await fs.readFile(filePath);
-  const parsedData = await parseStringPromise(data);
-  return createGameDefinitionFromParsedData(parsedData);
+  let dataFromFile;
+  let parsedData;
+
+  try {
+    dataFromFile = await fs.readFile(filePath);
+    parsedData = await parseStringPromise(dataFromFile);
+  } catch (error) {
+    throw new InvalidInputError('Cannot read or parsed input file.');
+  }
+
+  try {
+    return createGameDefinitionFromParsedData(parsedData);
+  } catch (error) {
+    if (error instanceof InvalidInputError) {
+      throw error;
+    }
+    throw new InvalidInputError('Invalid xml structure.');
+  }
 };
